f176de722a1b0352798e6e0c548c4775599f04fd
Ishwarya R <ishwarya.ad23@bitsathy.ac.in>
2025-12-15 20:20:15 +0530

    Modified

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Scans all JS/TS/HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
